671455def3009f40d0002e0a2fc3aad2fd2f9855
taycannon <taycannon2003@gmail.com>
2023-10-31 19:17:42 -0500

    Starting Posts.js

--- models/posts.js
+++ models/posts.js
@@ -1,2 +1,17 @@
 //Posts.js
 
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+  });
