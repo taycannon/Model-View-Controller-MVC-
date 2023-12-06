d7ed49579eedfaeecdd0eae02e71bd54446db52b
taycannon <taycannon2003@gmail.com>
2023-12-06 15:52:40 -0600

    restructured models

--- models/Comment.js
+++ models/Comment.js
@@ -1,15 +1,44 @@
-const { Sequelize, Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Comment extends Model {}
 
 Comment.init({
-    body: {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment_description: {
         type: DataTypes.STRING,
-        allowNull: false
-    }
-}, {
-    sequelize
-});
+      },
+      date_created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      blog_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'post',
+          key: 'id',
+        },
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'comment',
+    });
 
 module.exports = Comment;
