7c43b012c7683f2ef144606d35fb71b7d9644c23
taycannon <taycannon2003@gmail.com>
2023-12-06 16:13:35 -0600

    Tried to push to heroku still failed, got help for it but still did not work

--- models/Post.js
+++ models/Post.js
@@ -25,7 +25,7 @@ Post.init( {
     user_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'user', 
+        model: 'User', 
         key: 'id',
       },
     },
@@ -35,7 +35,7 @@ Post.init( {
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'post',
+    modelName: 'Post',
   });
 
 module.exports = Post;
